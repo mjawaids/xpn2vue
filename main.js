36ba3f7ee05ef5deb18a5648b0c81fdbf2622191
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2017-01-17 07:40:18 +0500

    Added row delete basic functionality

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 Vue.component('xpn-grid', {
+    props: { gridData: Array },
+
     template: `
     <table class="table">
         <tr v-for="row in gridData">
             <td v-for="el in row">
-                <xpn-element @addRow="addRow" @deleteRow="deleteRow">{{ el }}</xpn-element>
+                <xpn-element @addRow="addRow" @deleteRow="deleteRow" :el="el" :id="el.id">{{ el.type }} - {{ el.id }}</xpn-element>
             </td>
         </tr>
     </table>
     `,
-
-    props: { gridData: Array },
 
     data() {
         return {
@@ -23,30 +23,62 @@ Vue.component('xpn-grid', {
     
     methods: {
         addRow(atBottom=true) {
-            console.log(atBottom);
+
+            // TODO: add row based on updated number of columns
+
             if(atBottom) {
-                this.grid.push(['41', '42', '43']);
+                let bottomRow = [{id:'41',type:'message'}, {id:'42',type:'action'}, {id:'43',type:'request'}];
+                this.grid.push(bottomRow);
                 return;
             }
 
-            this.grid.unshift(['01', '02', '03']);
+            let topRow = [{id:'01',type:'response'}, {id:'02',type:'service'}, {id:'03',type:'task'}];
+            this.grid.unshift(topRow);
         },
 
-        deleteRow() {
-            alert('delete me');
+        deleteRow(obj) {
+            let foundRow = null;
+
+            for(let i = 0; i<this.grid.length; i++) {
+                for(let j = 0; j<this.grid[i].length; j++) {
+                    if(this.grid[i][j] == obj) {
+                        foundRow = true;
+                        break;
+                    }
+                }
+
+                if(foundRow) {
+                    // TODO: check condition for min grid with top, mid, and bottom rows
+                    this.grid.splice(i, 1);
+                    break;
+                }
+            }
         }
     }
 });
 
 Vue.component('xpn-element', {
+    props: ['id', 'el'],
     template: `
     <div>
         <div><slot></slot></div>
+        <input type='hidden' :value='id' />
         <button @click="$emit('addRow')">Add Bottom Row</button>
-        <button @click="$emit('addRow(false)')">Add Top Row</button>
-        <button @click="$emit('deleteRow')">X</button>
+        <button @click="$emit('addRow', false)">Add Top Row</button>
+        <button @click="$emit('deleteRow', el)">X</button>
     </div>
-    `
+    `,
+
+    data() {
+        return {
+            
+        }
+    },
+
+    // created() {
+    //     console.log("element: ");
+    //     console.log( this.el);
+    // },
 });
 
 Vue.use(VueMaterial);
@@ -54,9 +86,9 @@ new Vue({
     el: '#root',
     data: {
         tabledata: [
-            ['11', '12', '13'],
-            ['21', '22', '23'],
-            ['31', '32', '33']
+            [{id:'11',type:'response'}, {id:'12',type:'service'}, {id:'13',type:'task'}],
+            [{id:'21',type:'object'}, {id:'22',type:'rule'}, {id:'23',type:'subject'}],
+            [{id:'31',type:'message'}, {id:'32',type:'action'}, {id:'33',type:'request'}]
         ]
     },
     methods: {
