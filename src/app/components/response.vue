3eeaa9ec32a6d234ed268d1cbae153340e6eec4f
Nawazish <nawazishaliejaz@gmail.com>
2018-04-28 23:22:38 +0500

    1) Final implementation of Report form. 2) Some minor fixes. 3) Delete button implemented.

--- src/app/components/response.vue
+++ src/app/components/response.vue
@@ -13,7 +13,7 @@
             <md-option value="report">Report</md-option>
             <md-option value="?">?</md-option>
         </md-select>
-        <md-dialog v-if="el.value === 'report'" :ref="`dialog-${i}${j}`">
+        <md-dialog v-if="el.value === 'report'" :ref="`dialog-${i}${j}`" @close="onClose">
             <md-dialog-title class="text-center">Monthly Costs Action Details</md-dialog-title>
             <md-dialog-content class="bigger-modal response-modal-grid">
                 <div class="column col-left">
@@ -30,7 +30,23 @@
                     </md-input-container>
                 </div>
                 <div class="column">
-
+                    <div v-if="!clickedItem">Please select a object from side menu</div>
+                    <div v-else>
+                    <h4 class="text-center">Object Attributes</h4>
+                    <md-input-container>
+                        <label>Order</label>
+                        <md-input v-model="clickedItem.objectAttributes.order"></md-input>
+                    </md-input-container>
+                    <md-input-container>
+                        <label>Filter</label>
+                        <md-input v-model="clickedItem.objectAttributes.filter"></md-input>
+                    </md-input-container>
+                    <h4 class="text-center">Field Attributes</h4>
+                    <md-input-container v-for="(key, index) in fieldAttrKeys" :key="index">
+                        <label>{{key}}</label>
+                        <md-input v-model="clickedItem.fieldAttributes[key]"></md-input>
+                    </md-input-container>
+                    </div>
                 </div>
             </md-dialog-content>
 
@@ -52,7 +68,8 @@ export default {
     props: ['el', 'mode', 'i', 'j'],
     components: {treeList},
     data: () => ({
-        selectedObj: ''
+        selectedObj: '',
+        clickedItem: ''
     }),
     methods: {
       openDialog(ref) {
@@ -61,6 +78,9 @@ export default {
       },
       closeDialog(ref) {
         this.$refs[ref].close();
+      },
+      onClose() {
+        this.clickedItem = '';
       },
       saveAndCloseDialog(ref) {
         this.closeDialog(ref);
@@ -73,13 +93,25 @@ export default {
       },
       handleUnIndent(event){
         this.el.objects.push(event);
+      },
+      handleItemClicked(obj) {
+        this.clickedItem = obj;
       }
     },
     computed: {
       objects() {
         return this.$store.getters.getAllObjects;
+      },
+      fieldAttrKeys() {
+          if(this.clickedItem) return Object.keys(this.clickedItem.fieldAttributes);
+      }
+    },
+    created() {
+      let handleItemClicked = this.handleItemClicked;
+      this.$bus.on('itemClicked', function(obj){
+        handleItemClicked(obj);
+      });
     }
-  }
 }
 </script>
 
@@ -99,7 +131,7 @@ export default {
 .response-modal-grid {
     display: grid;
     grid-column-gap: 10px;
-    grid-template-columns: 400px 540px;
+    grid-template-columns: 400px auto;
     height: 400px;
 }
 
@@ -107,6 +139,7 @@ export default {
     border: solid 1px grey;
     border-radius: 5px; 
     padding: 5px;
+    overflow: scroll;
 }
 
 </style>
